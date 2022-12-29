2ae917b490cab4cec468b923f3078d9e14293626
Mati1432 <mateuszsliwinski9@gmail.com>
2022-12-29 18:48:08 +0100

    add validate main data

--- project/database.py
+++ project/database.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import sqlalchemy
-
-from validate_data import validate
+from validate_data import type_validate,check_nan_values
 
 
 def create_engine(name: str) -> sqlalchemy.create_engine:
@@ -10,15 +9,12 @@ def create_engine(name: str) -> sqlalchemy.create_engine:
 
 
 url_database = 'postgresql+psycopg2://postgres:admin@localhost/postgres'
-
 dbEngine = create_engine(url_database)
-
 connection = dbEngine.connect()
 
 df = pd.read_csv('data/wig20_d.csv')
-
-validate(df)
-
+df = type_validate(df)
+df = check_nan_values(df)
 if not connection.dialect.has_table(connection, 'wig'):
     df.to_sql('wig', dbEngine)
 
--- project/validate_data.py
+++ project/validate_data.py
@@ -1,16 +1,53 @@
 import urllib.request
-
+from datetime import datetime
 from pandas import DataFrame
+import pandas as pd
 
 
 def validate_download(url):
     try:
         url = urllib.request.urlretrieve(url, 'data/wig20_d.csv')
     except FileExistsError:
-        print("this url probably doesn't exist")
-
+        raise FileExistsError("This url probably doesn't exist")
     return url
 
 
-def validate(df: DataFrame) -> DataFrame:
-    pass
+def type_validate(df: DataFrame) -> DataFrame:
+    try:
+        pd.to_datetime(df['Data'], format='%Y-%m-%d', errors='raise')
+    except ValueError:
+        raise ValueError('Incorrect data format, should be YYYY-MM-DD')
+
+    try:
+        df['Otwarcie'] == df['Otwarcie'].astype(float)
+    except ValueError:
+        raise ValueError('Incorrect Otwarcie format, should be float')
+
+    try:
+        df['Najwyzszy'] == df['Najwyzszy'].astype(float)
+    except ValueError:
+        raise ValueError('Incorrect Najwyzszy format, should be float')
+
+    try:
+        df['Najnizszy'] == df['Najnizszy'].astype(float)
+    except ValueError:
+        raise ValueError('Incorrect Najnizszy format, should be float')
+
+    try:
+        df['Zamkniecie'] == df['Zamkniecie'].astype(float)
+    except ValueError:
+        raise ValueError('Incorrect Zamkniecie format, should be float')
+
+    try:
+        df['Wolumen'] == df['Wolumen'].astype(float)
+
+    except ValueError:
+        raise ValueError('Incorrect Wolumen format, should be float')
+    return df
+
+
+def check_nan_values(df: DataFrame) -> DataFrame:
+    if not df.isnull().values.any():
+        return df
+    else:
+        raise ValueError('Dataset have NaN values')
